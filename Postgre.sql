478a7dd641d4d8f9011eab6ac4a2d961e48e4a97
DelaiDan <arthurdelai@gmail.com>
2023-06-25 19:14:19 -0300

    SQL Example

--- Postgre.sql
+++ Postgre.sql
@@ -1,5 +1,22 @@
 -- Adminer 4.8.1 PostgreSQL 15.2 (Debian 15.2-1.pgdg110+1) dump
 
+DROP TABLE IF EXISTS "brands";
+DROP SEQUENCE IF EXISTS brands_id_seq;
+CREATE SEQUENCE brands_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE TABLE "public"."brands" (
+    "id" integer DEFAULT nextval('brands_id_seq') NOT NULL,
+    "name" character varying NOT NULL,
+    CONSTRAINT "brands_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+INSERT INTO "brands" ("id", "name") VALUES
+(1,	'Peugeot'),
+(4,	'Chevrolet'),
+(5,	'FIAT'),
+(6,	'Volkswagen'),
+(8,	'BMW');
+
 DROP TABLE IF EXISTS "vehicles";
 DROP SEQUENCE IF EXISTS vehicles_id_seq;
 CREATE SEQUENCE vehicles_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
@@ -12,18 +29,10 @@ CREATE TABLE "public"."vehicles" (
     CONSTRAINT "vehicle_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
-TRUNCATE "vehicles";
+INSERT INTO "vehicles" ("id", "model", "year", "brand") VALUES
+(1,	'308',	2013,	1),
+(8,	'Corsa Classic',	2009,	4),
+(9,	'Corsa JOY',	2006,	4),
+(10,	'E6',	1978,	8);
 
-DROP TABLE IF EXISTS "brands";
-DROP SEQUENCE IF EXISTS brands_id_seq;
-CREATE SEQUENCE brands_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-
-CREATE TABLE "public"."brands" (
-    "id" integer DEFAULT nextval('brands_id_seq') NOT NULL,
-    "name" character varying NOT NULL,
-    CONSTRAINT "brands_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
-
-TRUNCATE "brands";
-
--- 2023-05-21 18:28:23.74844+00
+-- 2023-06-25 22:13:45.258655+00
